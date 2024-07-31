693524ba92d224337bf1d4e7cb260be2c66913a0
rflores <104962825+rfloresj@users.noreply.github.com>
2024-07-30 21:21:27 -0600

    backend:add-allowed-origin-env-variable

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const serviceAccount = {
 
 const app = express();
 const corsOptions = {
-  origin: 'https://loquacious-sunshine-54eab5.netlify.app',
+  origin: process.env.ALLOWED_ORIGIN,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   optionsSuccessStatus: 200,
@@ -33,7 +33,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  connectTimeout: 10000 // 10 seconds
+  connectTimeout: 10000,
 });
 
 db.connect((err) => {
